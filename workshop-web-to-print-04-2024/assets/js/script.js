cc94fb87e9cb12495907f90b3f13329588a30db9
milenelaforge <hello@milenelaforge.com>
2024-11-19 15:51:29 +0100

    modify js

--- workshop-web-to-print-04-2024/assets/js/script.js
+++ workshop-web-to-print-04-2024/assets/js/script.js
@@ -41,34 +41,33 @@
                 });
         }
 
+
         // Définir la partie de l'URL qui est répétée
         const baseURL = 'https://raw.githack.com/milenelaforge/milenelaforge.github.io/main/';
 
-        // Créer les URLs individuelles en ajoutant le chemin spécifique de chaque fichier
-        const markdownFileURL1 = `${baseURL}workshop-web-to-print-04-2024/workshop-in-progress/sujet-ww2p-ensaama.md`;
-        const markdownFileURL2 = `${baseURL}workshop-web-to-print-04-2024/workshop-in-progress/programme-ww2p-ensaama.md`;
-        const markdownFileURL3 = `${baseURL}sources/web-to-print-theory/free-graphic-design/graphisme-libre-floss.md`;
-        const markdownFileURL4 = `${baseURL}sources/web-to-print-theory/free-graphic-design/publication-fluide.md`;
-        const markdownFileURL5 = `${baseURL}sources/web-to-print-theory/tools/liste-outils.md`;
-        const markdownFileURL6 = `${baseURL}sources/web-to-print-theory/tools/projets-web-to-print.md`;
-        const markdownFileURL7 = `${baseURL}sources/web-to-print-theory/tools/workshops-web-to-print.md`;
-        const markdownFileURL8 = `${baseURL}sources/web-to-print-theory/tools/trouver-des-reponses.md`;
-        const markdownFileURL9 = `${baseURL}sources/initiation-html-css/html-basics.md`;
-        const markdownFileURL10 = `${baseURL}sources/initiation-html-css/css-basics.md`;
-        const markdownFileURL11 = `${baseURL}sources/initiation-html-css/pratiquons.md`;
-        const markdownFileURL12 = `${baseURL}sources/initiation-html-css/medias-print.md`;
-        const markdownFileURL13 = `${baseURL}sources/initiation-html-css/paged-js.md`;
-        const markdownFileURL14 = `${baseURL}sources/initiation-html-css/paged-js-use.md`;
-        const markdownFileURL15 = `${baseURL}sources/web-to-print-practice/notice-typographie.md`;
-        const markdownFileURL16 = `${baseURL}sources/web-to-print-practice/fonderies-libres.md`;
-        const markdownFileURL17 = `${baseURL}sources/web-to-print-practice/notice-grille-compo.md`;
-        const markdownFileURL18 = `${baseURL}sources/web-to-print-practice/notice-images-videos.md`;
-        const markdownFileURL19 = `${baseURL}workshop-web-to-print-04-2024/edition-resultat-documentation/liens.md`;
-        const markdownFileURL20 = `${baseURL}workshop-web-to-print-04-2024/workshop-in-progress/fab.md`;
+        const markdownFileURL1 = `${baseURL}sources/web-to-print-theory/free-graphic-design/graphisme-libre-floss.md`;
+        const markdownFileURL2 = `${baseURL}sources/web-to-print-theory/free-graphic-design/publication-fluide.md`;
+        const markdownFileURL3 = `${baseURL}sources/web-to-print-theory/tools/liste-outils.md`;
+        const markdownFileURL4 = `${baseURL}sources/web-to-print-theory/tools/projets-web-to-print.md`;
+        const markdownFileURL5 = `${baseURL}sources/web-to-print-theory/tools/workshops-web-to-print.md`;
+        const markdownFileURL6 = `${baseURL}sources/web-to-print-theory/tools/trouver-des-reponses.md`; 
+        const markdownFileURL7 = `${baseURL}sources/initiation-html-css/html-basics.md`;
+        const markdownFileURL8 = `${baseURL}sources/initiation-html-css/css-basics.md`;
+        const markdownFileURL9 = `${baseURL}sources/initiation-html-css/pratiquons.md`;
+        const markdownFileURL10 = `${baseURL}sources/initiation-html-css/medias-print.md`;
+        const markdownFileURL11 = `${baseURL}sources/initiation-html-css/paged-js.md`;
+        const markdownFileURL12 = `${baseURL}sources/initiation-html-css/paged-js-use.md`;
+        const markdownFileURL13 = `${baseURL}sources/web-to-print-practice/notice-typographie.md`;
+        const markdownFileURL14 = `${baseURL}sources/web-to-print-practice/fonderies-libres.md`;
+        const markdownFileURL15 = `${baseURL}sources/web-to-print-practice/notice-grille-compo.md`;
+        const markdownFileURL16 = `${baseURL}sources/web-to-print-practice/notice-images-videos.md`;
+        const markdownFileURL17 = `${baseURL}workshop-web-to-print-04-2024/workshop-in-progress/sujet-ww2p-ensaama.md`;
+        const markdownFileURL18 = `${baseURL}workshop-web-to-print-04-2024/workshop-in-progress/programme-ww2p-ensaama.md`;
+        const markdownFileURL19 = `${baseURL}workshop-web-to-print-04-2024/workshop-in-progress/workshop-in-progress.md`;
+        const markdownFileURL20 = `${baseURL}workshop-web-to-print-04-2024/edition-resultat-documentation/liens.md`;
         const markdownFileURL21 = `${baseURL}workshop-web-to-print-04-2024/edition-resultat-documentation/html.md`;
         const markdownFileURL22 = `${baseURL}workshop-web-to-print-04-2024/edition-resultat-documentation/css.md`;
         const markdownFileURL23 = `${baseURL}workshop-web-to-print-04-2024/edition-resultat-documentation/retour-en-images.md`;
-
 
         // Appeler la fonction pour charger le contenu dans différentes div
         loadMarkdownIntoDiv(markdownFileURL1, 'graphisme-libre-floss');
